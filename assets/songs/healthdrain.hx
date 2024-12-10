8e5717897e0f83e7c34ee9745f6632b42945e0e9
Danny <67078432+DiGear@users.noreply.github.com>
2024-12-10 02:18:46 -0500

    add varying levels of health drain

--- assets/songs/healthdrain.hx
+++ assets/songs/healthdrain.hx
@@ -1,7 +1,13 @@
 //
-function onDadHit(NoteHitEvent){
-    if(Options.globalHealthDrain)
-    {
-        health = Math.max(0.001, health - 0.02);
+function onDadHit(NoteHitEvent) {
+    switch (Options.globalHealthDrain) {
+        case "off":
+            return;
+        case "normal":
+            health = Math.max(0.001, health - 0.017);
+        case "hard":
+            health = Math.max(0.001, health - 0.02);
+        case "unfair":
+            health = Math.max(0.001, health - 0.025);
     }
 }
