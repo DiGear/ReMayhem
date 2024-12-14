ee78f50627fddfcf4132e1518a7c739181ae0697
Sam <thegamemaster210@gmail.com>
2024-12-14 03:38:02 -0600

    night tint is now autoloaded on danny2

--- assets/songs/night_tint.hx
+++
@@ -1,8 +0,0 @@
-//
-function create() {
-	if (SONG.meta.displayName.toLowerCase() = "stargazer" || SONG.meta.displayName.toLowerCase() = "singularity") { // add more songs to night tint here
-    boyfriend.setColorTransform(0.5, 0.5, 0.8);
-	dad.setColorTransform(0.5, 0.5, 0.8);
-	gf.setColorTransform(0.5, 0.5, 0.8);
-	}
-}
